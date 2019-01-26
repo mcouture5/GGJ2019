e101e71cbd07970dbea060ef2d9a81371f679853
Benjamin Dodge <benjamin9dodge@gmail.com>
2019-01-25 21:14:01 -0500

    Adding stubs for BinaryInputThingy.

---
+++ src/objects/binary-input/BinaryInputThingy.ts
@@ -0,0 +1,9 @@
+export class BinaryInputThingy extends Phaser.GameObjects.Group {
+
+    constructor(params: {scene: Phaser.Scene}) {
+        super(params.scene);
+    }
+
+    public update() {
+    }
+}
